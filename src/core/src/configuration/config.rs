0cdb3d26fa469ae4f6f03ca31f633348712a4ae5
EstebanGiorgis <145534788+EstebanGiorgis@users.noreply.github.com>
2025-08-28 17:07:25 +0200

    feat: implement controller_handler without all attribut, but the three functions of the class diagram have been implemented

--- src/core/src/configuration/config.rs
+++ src/core/src/configuration/config.rs
@@ -1,17 +1,27 @@
-use super::configuration;
+use log::info;
+use crate::error_handling::types::*;
 
 /// Represents the configuration for the application.
 /// This struct is currently empty but can be extended to include
 /// fields and methods for managing application settings.
-struct Configuration {}
+pub struct Config {
+    foo: u16,
+}
 
-impl Configuration {
+impl Config {
     /// Creates a new instance of `Configuration`.
     ///
     /// # Returns
     /// A new `Configuration` instance.
-    pub fn new() -> Configuration {
-        Configuration {}
+    pub fn from_file() -> Result<Config, ConfigError> {
+       info!("Configuration file loaded from the TOML");
+        Ok(Config { foo: 5 })
     }
+
+    pub fn from_args() -> Result<Config, ConfigError> {
+        info!("Configuration loaded from the command-line arguments");
+        Ok(Config {foo: 5})
+    }
+
 }
 
--- src/core/src/controller/controller_handler.rs
+++ src/core/src/controller/controller_handler.rs
@@ -1,23 +1,53 @@
+use log::{debug, error, trace, warn, info};
+use crate::configuration::config::Config;
+use crate::error_handling::types::*;
+use std::path::PathBuf;
+use std::env;
 pub struct Controller {
     // Fields for the Controller struct
-    pub foo: i32,
+    pub config: Config,
 }
 
-// Constructor synatx
+// Constructor syntax
 impl Controller {
-    pub fn new() -> Controller {
-        Controller { foo: 0 }
+    // Returns nothing for the moment
+    pub fn new() -> Result<Self, ControllerError> {
+        info!("[+] INFO: called `new()` function in Controller");
+
+        // If more than one arg is present that means flags were added to the startup command
+        if env::args().len() > 1 {
+            match Config::from_args() {
+                Ok(config) =>  Ok(Self { config }),
+                Err(err) => {
+                   error!("[!]ERROR: {:?}", err);
+                    Err(ControllerError::Config(err))
+                }
+            }
+        } else { // Loading config from the file
+            match Config::from_file() {
+                Ok(config) => Ok(Self { config }),
+                Err(err) => {
+                    error!("[!] ERROR: {:?}", err);
+                    Err(ControllerError::Config(err))
+                }
+            }
+        }
+    }
+
+    pub fn run() {
+        info!("[+] INFO: called `run()` function in Controller");
+    }
+
+    pub fn shutdown()  {
+        info!("[+] INFO: called `shutdown()` function in Controller");
     }
 }
 
-fn private_function() {
-    // This function is private to the module
-}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_new_with_args() {
 
-pub mod nested {
-    pub fn nested_function() {
-        // This function is public within the nested module
     }
-}
 
-impl Controller {}
+}
