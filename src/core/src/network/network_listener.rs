67527a0fa530601c6e06db2ae5acf3b40e344163
nilsdonatantonio <nils.donatantonio@heig-vd.ch>
2025-08-29 11:08:18 +0200

    feat: initial commit
    Setup `NetworkListener` structure adding minimal code needed for the
    types and fields
    
    Import of tokio crate

--- src/core/src/network/network_listener.rs
+++ src/core/src/network/network_listener.rs
@@ -1,6 +1,24 @@
+use super::connection_filter::*;
+use super::service_detector::*;
+use super::session_request::*;
+use crate::configuration::types::ServiceConfig;
+use crate::error_handling::types::NetworkError;
+
+use std::collections::HashMap;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc::Sender;
+
 pub struct NetworkListener {
-    // Fields for the ListenerManager struct
-    pub foo: i32,
+    listeners: HashMap<u16, TcpListener>,
+    session_tx: Sender<SessionRequest>,
+    service_detector: ServiceDetector,
+    connection_filter: ConnectionFilter,
 }
 
-impl NetworkListener {}
+impl NetworkListener {
+    pub fn new() -> Self {}
+    pub fn bind_services(service: &[ServiceConfig]) {}
+    pub fn start_listening(session_tx: Sender<SessionRequest>) -> Result<(), NetworkError> {}
+    pub fn shutdown() -> Result<(), NetworkError> {}
+    fn handle_connection(stream: TcpStream, service: &str) {}
+}
