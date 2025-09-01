e35e8a2bebd4d9fec374c28893bf7bd831120fc0
nilsdonatantonio <nils.donatantonio@heig-vd.ch>
2025-09-01 11:43:47 +0200

    feat: implements ConnectionFilter
    Implements methods from the ConnectionFilter
    
    is_connection_allowed() is called from NetworkListener and uses
    is_ip_allowed and is_port_allowed internally
    
    is_ip_allowed decides depending on the whitelist_default field
    
    is_port_allowed is blacklist by default

--- src/core/src/network/connection_filter.rs
+++ src/core/src/network/connection_filter.rs
@@ -1,10 +1,14 @@
 use super::protocol_filter::ProtocolFilter;
 use crate::configuration::types::{IpFilter, PortFilter};
 
+use std::net::IpAddr;
+
+#[derive(Clone)]
 pub struct ConnectionFilter {
     ip_filter: IpFilter,
     port_filter: PortFilter,
-    protocol_filter: ProtocolFilter,
+    protocol_filter: ProtocolFilter, // What is the purpose of this field if it's not passed to
+                                     // new() ?
 }
 
 impl Default for ConnectionFilter {
@@ -14,5 +18,67 @@ impl Default for ConnectionFilter {
             port_filter: PortFilter::default(),
             protocol_filter: ProtocolFilter::default(),
         }
+    }
+}
+
+impl ConnectionFilter {
+    pub fn new(ip_filter: IpFilter, port_filter: PortFilter) -> Self {
+        Self {
+            ip_filter,
+            port_filter,
+            protocol_filter: ProtocolFilter::default(),
+        }
+    }
+    pub fn should_accept_connection(&self, client_addr: &IpAddr, port: u16) -> bool {
+        if !(self.is_ip_allowed(client_addr) && self.is_port_allowed(port)) {
+            return false;
+        }
+        true
+    }
+
+    fn is_ip_allowed(&self, ip: &IpAddr) -> bool {
+        let mut result = true;
+        if self.ip_filter.whitelist_mode {
+            // White list mode
+            for ip_range in self.ip_filter.allowed_ranges.iter() {
+                if !(ip.min(&ip_range.start).eq(&ip_range.start)
+                    && ip.max(&ip_range.end).eq(&ip_range.end))
+                {
+                    result = false;
+                }
+            }
+            result
+        } else {
+            // Blacklist mode
+            for ip_range in self.ip_filter.blocked_ranges.iter() {
+                if ip.min(&ip_range.start).eq(&ip_range.start)
+                    && ip.max(&ip_range.end).eq(&ip_range.end)
+                {
+                    result = false;
+                }
+            }
+            result
+        }
+    }
+
+    // Blacklists by default
+    fn is_port_allowed(&self, port: u16) -> bool {
+        let mut result = true;
+        for port_range in self.port_filter.allowed_ports.iter() {
+            if !(port.min(port_range.start) == port_range.start
+                && port.max(port_range.end) == port_range.end)
+            {
+                result = false;
+            }
+        }
+
+        for port_range in self.port_filter.blocked_ports.iter() {
+            if port.min(port_range.start) == port_range.start
+                && port.max(port_range.end) == port_range.end
+            {
+                result = false;
+            }
+        }
+        result
     }
 }
