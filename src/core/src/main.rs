e8a96eb5974c358bb556bb87d3e4baf370b9a8ca
nilsdonatantonio <nils.donatantonio@heig-vd.ch>
2025-09-03 16:10:40 +0200

    feat: implement program startup and run loop
    `main.rs` shows a welcome message and logging to track the boot process of the app and possible errors

--- src/core/src/main.rs
+++ src/core/src/main.rs
@@ -1,18 +1,85 @@
-use log::{debug, error, trace, warn};
+use clap::Parser;
+use log::{error, info};
+use miel::configuration::config::Config;
+use miel::controller::controller_handler::Controller;
+use std::path::Path;
 
-fn main() {
+#[derive(Parser)]
+#[command(name = "miel")]
+#[command(version = "0.0.1")]
+#[command(about = "A comprehensive Chameleon Research Honeypot")]
+struct Args {
+    config_file: String,
+}
+
+#[tokio::main]
+async fn main() {
     // Example how to log
     // https://docs.rs/env_logger/latest/env_logger/
-    env_logger::init();
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .format_target(false)
+        .init();
 
-    //let e = ConfigError::InvalidFormat;
-    // info!("informing {:?}", e); -> will only work if the enum implement Debug (add #[derive(Debug) to the enum]
-    debug!("debugging");
-    warn!("warning");
-    error!("error");
-    trace!("trace");
+    println!(
+        "
+██████╗  ██████╗  ██████╗ █████╗ ██╗         ██╗███╗   ███╗██╗███████╗██╗     
+██╔══██╗██╔═████╗██╔════╝██╔══██╗██║        ██╔╝████╗ ████║██║██╔════╝██║     
+██████╔╝██║██╔██║██║     ███████║██║       ██╔╝ ██╔████╔██║██║█████╗  ██║     
+██╔══██╗████╔╝██║██║     ██╔══██║██║      ██╔╝  ██║╚██╔╝██║██║██╔══╝  ██║     
+██████╔╝╚██████╔╝╚██████╗██║  ██║███████╗██╔╝   ██║ ╚═╝ ██║██║███████╗███████╗
+╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝    ╚═╝     ╚═╝╚═╝╚══════╝╚══════╝
+==============================================================================
+              A comprehensive Chameleon Research Honeypot v0.0.1              
+==============================================================================
+"
+    );
 
-    // run with RUST_LOG=[target]=[level] cargo run, ex: RUST_LOG=miel cargo run -> displays every type of log
+    info!("Importing configuration");
+
+    // Get command-line arguments
+    let args = Args::parse();
+
+    if args.config_file.is_empty() {
+        error!("No configuration file found");
+        std::process::exit(1);
+    }
+
+    let config = Config::from_file(Path::new(args.config_file.as_str())).map_err(|e| {
+        error!("Unable to import configuration from file: {:?}", e);
+        std::process::exit(1);
+    });
+
+    info!("Configuration imported successfully");
+
+    let mut controller = Controller::new(config.unwrap())
+        .map_err(|e| {
+            error!(
+                "Unable to create a controller instance: {:?}, exiting...",
+                e
+            );
+            std::process::exit(1);
+        })
+        .unwrap();
+
+    let result = tokio::spawn(async move {
+        info!("Spawning the controller");
+        controller
+            .run()
+            .await
+            .map_err(|e| {
+                error!(
+                    "Error occured in the controller process: {:?}, exiting...",
+                    e
+                )
+            })
+            .unwrap();
+    });
+
+    let _ = result.await.map_err(|e| {
+        error!("Error joining at the end of execution: {:?}", e);
+        std::process::exit(1);
+    });
 }
 
 #[cfg(test)]
